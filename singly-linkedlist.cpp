e7262a346befda9029d90b1303bff5e87a6b9d44
sekar243 <kinasekin6602@gmail.com>
2025-05-06 12:08:59 +0700

    case 5

--- singly-linkedlist.cpp
+++ singly-linkedlist.cpp
@@ -187,7 +187,16 @@ int main()
             }
         }
         break;
-     
+        case '5':
+        {
+            exit(0);
+        }
+        break;
+        default:
+        {
+            cout << "Pilihan salah !." << endl;
+        }
+        break;
   
         }
 
