08b6f2f14e95ec74182610d1052e3a67dcf6c759
sekar243 <kinasekin6602@gmail.com>
2025-05-06 12:07:28 +0700

    case 2

--- singly-linkedlist.cpp
+++ singly-linkedlist.cpp
@@ -139,6 +139,26 @@ int main()
         }
         break;
         
+        case '2':
+        {
+            if (mhs.listEmpty())
+            {
+                cout << endl
+                     << "List Kosong" << endl;
+                break;
+            }
+            cout << endl 
+                 << "\nMasukkan no mahasiswa yang akan dihapus : ";
+            cin >> nim;
+            if (mhs.delNode(nim) == false )
+                cout << endl
+                << "Data tidak ditemukan" << endl;
+            else 
+                cout << endl 
+                     << "Data dengan nomer mahasiswa " << nim
+                     << " berhasil dihapus " << endl;
+        }
+
   
         }
 
