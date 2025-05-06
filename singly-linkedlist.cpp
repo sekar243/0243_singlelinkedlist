f3905b76fcbd4caad26bc6ccf62430a74dcc3f9d
sekar243 <kinasekin6602@gmail.com>
2025-05-06 12:24:21 +0700

    menambah tampilan menu

--- singly-linkedlist.cpp
+++ singly-linkedlist.cpp
@@ -132,5 +132,27 @@ public:
     }
 };
 
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+    while (1)
+    {
+        cout << endl
+             << "Menu";
+        cout << endl
+             << "1. Menambahb data kedalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << endl
+             << "Masukkan pilihan (1-5): ";
+        cin >> ch;
+        
+    }
+}
 
+}
 
